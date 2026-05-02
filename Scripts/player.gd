9fbaff406eb268263556c64bdd5377fe47dfb946
MalekDeKalem <dimitri200169@gmail.com>
2026-05-02 23:12:53 +0200

    animation2dsprite

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,8 +3,9 @@ extends CharacterBody2D
 @export var gravity = 30
 @export var jump_force = 300
 @export var weapon_scene: PackedScene
+@export var running_speed = 150
 
-
+@onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	if !is_on_floor():
@@ -13,12 +14,26 @@ func _physics_process(delta: float) -> void:
 			velocity.y=1000;
 	
 	var Horizontal_direction = Input.get_axis("move left","move right")
-	velocity.x=speed * Horizontal_direction
-	
+	var curr_speed = speed
+	if Input.is_action_pressed("run"):
+		curr_speed = speed + running_speed
+	velocity.x= curr_speed * Horizontal_direction
+
 	if Input.is_action_just_pressed("jump") && is_on_floor():
 		velocity.y = -jump_force
-		
-			
-	
+
 	move_and_slide()
+	
+	if !is_on_floor():
+		anim.play("jump")
+		anim.flip_h = Horizontal_direction < 0
+	elif Horizontal_direction != 0:
+		if Input.is_action_pressed("run"):
+			anim.play("walk")
+		else:
+			anim.play("walk")
+		anim.flip_h = Horizontal_direction < 0
+	else:
+		anim.stop()
+
 	print(velocity)
